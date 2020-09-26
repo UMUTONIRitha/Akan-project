549c21441cd8df93cff909df61f5961b7fbd1d4c
UMUTONIRitha <rithamarie9@gmail.com>
2020-09-26 19:44:26 +0200

    add if condition and list of days

--- scripts.js
+++ scripts.js
@@ -2,4 +2,7 @@ function getname(){
     var gender=document.getElementById("gender").checked;
     var day=document.getElementById("birthday").value;
     var date= new Date(day).getDay(); 
+    if(day!==null){
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"][date];
+}
 }
