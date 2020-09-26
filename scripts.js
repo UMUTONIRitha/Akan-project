3799bd6506b5e97ec942a8e632f60623a2bb84ea
UMUTONIRitha <rithamarie9@gmail.com>
2020-09-26 19:47:17 +0200

    add if condition, to check gender and list of names for female

--- scripts.js
+++ scripts.js
@@ -4,5 +4,9 @@ function getname(){
     var date= new Date(day).getDay(); 
     if(day!==null){
         var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"][date];
-}
+        if(gender==true){
+            var names = ["Kwasi","Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+                    var getname = names[date];
+        }
+    }
 }
