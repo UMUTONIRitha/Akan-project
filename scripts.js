da4c221b180845f48b34efb2256db2c257a3e19d
UMUTONIRitha <rithamarie9@gmail.com>
2020-09-26 20:09:38 +0200

    add logic operator and

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function getname(){
     var gender=document.getElementById("gender").checked;
     var day=document.getElementById("birthday").value;
     var date= new Date(day).getDay(); 
-    if(day!==null){
+    if(day&&gender!==null){
         var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"][date];
         if(gender==true){
             var names = ["Kwasi","Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
