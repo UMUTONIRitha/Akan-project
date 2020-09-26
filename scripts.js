b285ff22c3de56c6d8aa0dd9c8bf5cba43a52137
UMUTONIRitha <rithamarie9@gmail.com>
2020-09-26 20:20:50 +0200

    add indentation

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
-function getname(){
-    var gender=document.getElementById("gender").checked;
-    var day=document.getElementById("birthday").value;
-    var date= new Date(day).getDay(); 
-    if(day&&gender!==null){
-        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday","Saturday"][date];
-        if(gender==true){
-            var names = ["Kwasi","Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-                    var getname = names[date];
+function getname() {
+    var gender = document.getElementById("gender").checked;
+    var day = document.getElementById("birthday").value;
+    var date = new Date(day).getDay();
+    if (day && gender !== null) {
+        var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date];
+        if (gender == true) {
+            var names = ["Kwasi", "Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+            var getname = names[date];
         }
-        else{
+        else {
             var names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-                    var getname = names[date];
+            var getname = names[date];
         }
         alert("Hi, Your Akan Name is " + getname + ". the day you were born is " + days);
     }
-    else{
+    else {
         alert("Enter a date and select gender");
     }
 }
