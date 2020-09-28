37ed4c744e112e62c345e5f580af7c779805629b
UMUTONIRitha <rithamarie9@gmail.com>
2020-09-28 13:21:19 +0200

    add changes and indentation

--- scripts.js
+++ scripts.js
@@ -5,11 +5,12 @@ function getname() {
     if (day && gender !== null) {
         var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"][date];
         if (gender == true) {
-            var names = ["Kwasi", "Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+            var names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
             var getname = names[date];
         }
         else {
-            var names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+            var names = ["Kwasi", "Kwadwo", "kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
             var getname = names[date];
         }
         alert("Hi, Your Akan Name is " + getname + ". the day you were born is " + days);
